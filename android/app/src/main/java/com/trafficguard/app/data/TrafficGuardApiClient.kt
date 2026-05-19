e63be8482fecec4681ed9bd41eacc7605dd5c90f
Muhammad Ahmad Shahid <mahmads0323@gmail.com>
2026-05-19 08:10:20 +0500

    Fixed android connection

--- android/app/src/main/java/com/trafficguard/app/data/TrafficGuardApiClient.kt
+++ android/app/src/main/java/com/trafficguard/app/data/TrafficGuardApiClient.kt
@@ -244,7 +244,9 @@ object TrafficGuardApiClient {
      * - Emulator → http://10.0.2.2:8000/
      * - Physical device on same LAN → http://<your_machine_ip>:8000/
      */
-    const val BASE_URL = "http://10.0.2.2:8000/"
+    // const val BASE_URL = "http://10.0.2.2:8000/"
+    const val BASE_URL = "http://127.0.0.1:8000/"
+
 
     private val loggingInterceptor = HttpLoggingInterceptor { msg ->
         Log.d("OkHttp", msg)
